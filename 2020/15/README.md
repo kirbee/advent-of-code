4f42bd3937fe5d1cee566efbf01ae43a6f4d49e0
Kirbee Parsons <kirbeeleanne@gmail.com>
2020-12-15 19:03:39 -0800

    day 15 2020

--- 2020/15/README.md
+++ 2020/15/README.md
@@ -1,5 +1,5 @@
 # Advent of Code 2020
-## Day 14
+## Day 15
 
 To run (while in this directory):
-`node solution.js [filename]` where filename is the filename to open (default to input.txt in this directory)
+`node solution.js`
--- 2020/15/solution.js
+++ 2020/15/solution.js
@@ -4,12 +4,13 @@ const getNthSpoken = (startingNumbers, n) => {
   const lastSaid = {};
 
   const addToLastSaid = (number, turn) => {
-    if (lastSaid[number]) {
-      if (lastSaid[number].length === 2) {
-        const [timeBefore, mostRecent] = lastSaid[number];
+    const lastSaidValue = lastSaid[number];
+    if (lastSaidValue) {
+      if (lastSaidValue.length === 2) {
+        const [timeBefore, mostRecent] = lastSaidValue;
         lastSaid[number] = [mostRecent, turn];
       } else {
-        lastSaid[number] = [...lastSaid[number], turn];
+        lastSaid[number] = [...lastSaidValue, turn];
       }
     } else {
       lastSaid[number] = [turn];
@@ -27,11 +28,52 @@ const getNthSpoken = (startingNumbers, n) => {
     }
     addToLastSaid(thisTurn, i);
     last = thisTurn;
-    if (i % 100000 === 0) console.log(i, last, Object.keys(lastSaid).length);
+  }
+  return last;
+};
+
+const getNthSpokenMap = (startingNumbers, n) => {
+  const lastSaid = new Map();
+
+  const addToLastSaid = (number, turn) => {
+    const lastSaidValue = lastSaid.get(number);
+    if (lastSaidValue) {
+      if (lastSaidValue.length === 2) {
+        const [timeBefore, mostRecent] = lastSaidValue;
+        lastSaid.set(number, [mostRecent, turn]);
+      } else {
+        lastSaid.set(number, [...lastSaidValue, turn]);
+      }
+    } else {
+      lastSaid.set(number, [turn]);
+    }
+  };
+
+  startingNumbers.forEach((item, i) => addToLastSaid(item, i));
+
+  let last = startingNumbers.slice(-1).pop();
+  for (let i = startingNumbers.length; i < n; i++) {
+    let thisTurn = 0;
+    const lastSaidValue = lastSaid.get(last);
+    if (lastSaidValue.length == 2) {
+      const [timeBefore, lastTime] = lastSaidValue;
+      thisTurn = lastTime - timeBefore;
+    }
+    addToLastSaid(thisTurn, i);
+    last = thisTurn;
   }
   return last;
 };
 
 console.log('Part 1:', getNthSpoken(input, 2020));
+
 // This takes forever, presumably because I'm storing a trillion keys in a single object
+// ~500s
+console.time('object');
 console.log('Part 2:', getNthSpoken(input, 30000000));
+console.timeEnd('object');
+
+// Yes, the map is much faster (~30s)
+console.time('map');
+console.log('Part 2:', getNthSpokenMap(input, 30000000));
+console.timeEnd('map');
